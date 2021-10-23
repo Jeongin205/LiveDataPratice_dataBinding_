33e91b58f5ea73bca631cf0ed304d9e04befc7b1
Jeongin205 <81507170+Jeongin205@users.noreply.github.com>
2021-10-23 15:30:50 +0900

    Initial commit

---
+++ app/src/main/java/com/example/livedatapratice/FirstFragment.kt
@@ -0,0 +1,46 @@
+package com.example.livedatapratice
+
+import android.content.ContentValues.TAG
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+
+class FirstFragment : Fragment() {
+    private lateinit var vm : MainViewModel
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        vm = ViewModelProvider(requireActivity())[MainViewModel::class.java]
+        view.findViewById<TextView>(R.id.editText).addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                vm.inputText.value = p0.toString()
+            }
+
+        })
+    }
+
+
+}
